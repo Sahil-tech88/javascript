aeed44be2efc980c050469a2acf2f1ee35ecc898
Sahil-tech88 <sahilrajput8085@gmail.com>
2024-08-13 19:44:19 +0000

    container config done

--- 0_basics/test.js
+++ 0_basics/test.js
@@ -1,1 +1,12 @@
-console.log ("Sahil");
+const accountId = 454545
+let accountEmail = "sahilrajput8085gmail.com"
+var accountPassword = "my@name"
+accountCity = "Mohali"
+
+
+
+// accountId = 2 Not allowed 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
